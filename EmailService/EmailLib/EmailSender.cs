3bdb1cddbcca896a06937d88f4a55138d8a311bb
ABOLADE BOLU MARVELOUS <130442240+Marvelous-iAcademy@users.noreply.github.com>
2023-09-01 02:24:11 +0100

    Add files via upload

---
+++ EmailService/EmailLib/EmailSender.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace EmailLib
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailConfiguration _emailConfig;
+        public EmailSender(EmailConfiguration emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+            SendEmail(emailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(Message message) // Use MimeMessage instead of Message
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("email", _emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+            return emailMessage;
+        }
+        private void SendEmail(MimeMessage mailMessage) // Use MimeMessage instead of Message
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+                    client.Send(mailMessage);
+                }
+                catch
+                {
+                    //log an error message or throw an exception or both.
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
